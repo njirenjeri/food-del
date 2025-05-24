a04b1eaf3b8413118e0577d3c1e906d60c178d35
Njire Njeri <njirenjeri@gmail.com>
2025-05-24 10:59:10 +0300

    initial commit

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div className=''>
+        <Header/>
+        
+    </div>
+  )
+}
+
+export default Home
