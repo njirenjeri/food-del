e26a39cc73dfe2942c81686c55f6117de155db28
Njire Njeri <njirenjeri@gmail.com>
2025-06-18 20:10:04 +0300

    add food to menu

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/sideBar/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import Add from '../src/pages/AddItem/Add'
+import List from '../src/pages/List/List'
+import Orders from '../src/pages/Orders/Orders'
+
 
 const App = () => {
   return (
@@ -9,6 +14,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>} />
+          <Route path='/list' element={<List/>} />
+          <Route path='/orders' element={<Orders/>} />
+        </Routes>
       </div>
     </div>
   )
--- admin/src/components/sideBar/Sidebar.jsx
+++ admin/src/components/sideBar/Sidebar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets.js'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar-options">
-            <div className="sidebar-option">
+            <NavLink to='/add' className="sidebar-option">
                 <img src={assets.add_icon} alt="" />
                 <p>add items</p>
-            </div>
-            <div className="sidebar-option">
+            </NavLink>
+            <NavLink to='/list' className="sidebar-option">
                 <img src={assets.order_icon} alt="" />
                 <p>list items</p>
-            </div>
-            <div className="sidebar-option">
+            </NavLink>
+            <NavLink to='/orders' className="sidebar-option">
                 <img src={assets.order_icon} alt="" />
                 <p>orders</p>
-            </div>
+            </NavLink>
         </div>
     </div>
   )
